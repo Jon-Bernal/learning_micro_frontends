409bba12f95ff765628b8c213a79962e98cc7393
Jon Bernal <jon.bernal@guildeducation.com>
2022-04-14 09:15:38 -0700

    project 2 marketing and container are now connected.

--- Project2/packages/marketing/src/bootstrap.js
+++ Project2/packages/marketing/src/bootstrap.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+import App from './App'
 
 // mount function to startup the app
 const mount = el => {
   ReactDOM.render(
-    <h1>Hi there!</h1>,
+    <App />,
     el
   )
 }
 
 // if we are in dev and in isolation mode,
 // call mount immediately
+if(process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
+
+  if(devRoot){
+    mount(devRoot);
+  }
+}
 
 // we are running through container
 // and we should export the mount function
+export {mount}
